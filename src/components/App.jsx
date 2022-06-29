9303c3bd0d59ab83bb16fd1665349ed63be10930
Samuel Abioye <101730798+AbioyeSamuel@users.noreply.github.com>
2022-06-29 15:39:44 +0100

    inputText

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [items, setItems] = useState([]);
+
+  function handleChange(event) {
+    const newValue = event.target;
+    setInputText(newValue);
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
+        <input
+          onChange={handleChange}
+          type="text"
+          name="item"
+          value={inputText}
+        />
         <button>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item</li>
+          <li>Add Item</li>
         </ul>
       </div>
     </div>
