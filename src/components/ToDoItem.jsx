4d146c6daac9b7b33678fb9d3d4a692657a77dea
Samuel Abioye <101730798+AbioyeSamuel@users.noreply.github.com>
2022-08-22 02:01:05 -0700

    Update ToDoItem.jsx

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
 function ToDoItem(props) {
+  function removeItem(){
+    props.onChecked(props.id);
+  }
   return (
-    <div
-      onClick={() => {
-        props.onChecked(props.id);
-      }}
-    >
-      <li> {props.text}</li>;
+    <div>
+      <li onClick = {removeItem}> {props.text} </li>
     </div>
   );
 }
+
 export default ToDoItem;
